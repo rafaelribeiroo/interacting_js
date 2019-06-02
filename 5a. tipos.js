0d3ff3cf30c7daa6d2420716861a1dcc29d36ff8
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2019-06-02 20:05:11 -0300

    Aulas feitas até o presente momento

---
+++ 5a. tipos.js
@@ -0,0 +1,15 @@
+var (ou let in modern JS) n1 = 5
+var n2 = 8.5
+var n3 = 87
+
+var s1 = 'Curso'
+var s2 = "de"
+var s3 = `JS`
+
+var v = true
+var f = false
+
+typeof s1  // string
+typeof NaN  // number
+typeof Infinity  // number
+typeof null  // It's an object
